e303d5e0ae87ca68d672d130604ac8181a2ab1bc
Michael Henrique <oliveirasantos.mh@gmail.com>
2020-04-21 10:38:31 -0300

    feat: controller implementation - DONE

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,43 +1,17 @@
 #include "controller.h"
 
-#include <iostream>
-
 #include "SDL.h"
-#include "snake.h"
+#include "paddle.h"
 
-void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
-                                 Snake::Direction opposite) const {
-  if (snake.direction != opposite || snake.size == 1) snake.direction = input;
-  return;
-}
-
-void Controller::HandleInput(bool &running, Snake &snake) const {
+void Controller::handleInput(bool &running, Paddle &paddle) const {
   SDL_Event e;
   while (SDL_PollEvent(&e)) {
     if (e.type == SDL_QUIT) {
       running = false;
-    } else if (e.type == SDL_KEYDOWN) {
-      switch (e.key.keysym.sym) {
-        case SDLK_UP:
-          ChangeDirection(snake, Snake::Direction::kUp,
-                          Snake::Direction::kDown);
-          break;
-
-        case SDLK_DOWN:
-          ChangeDirection(snake, Snake::Direction::kDown,
-                          Snake::Direction::kUp);
-          break;
-
-        case SDLK_LEFT:
-          ChangeDirection(snake, Snake::Direction::kLeft,
-                          Snake::Direction::kRight);
-          break;
-
-        case SDLK_RIGHT:
-          ChangeDirection(snake, Snake::Direction::kRight,
-                          Snake::Direction::kLeft);
-          break;
-      }
+    } else {
+      const Uint8 *state = SDL_GetKeyboardState(NULL);
+      if (state[SDL_SCANCODE_UP]) paddle.move(true);
+      if (state[SDL_SCANCODE_DOWN]) paddle.move(false);
     }
   }
 }
