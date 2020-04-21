a868dbc9ac0d0037ab94b2117cddfe0a5db21cfa
Michael Henrique <oliveirasantos.mh@gmail.com>
2020-04-21 12:01:12 -0300

    Controller Updated

--- src/controller.cpp
+++ src/controller.cpp
@@ -3,15 +3,15 @@
 #include "SDL.h"
 #include "paddle.h"
 
-void Controller::handleInput(bool &running, Paddle &paddle) const {
+void Controller::handleInput(bool &running, Paddle &p) const {
   SDL_Event e;
   while (SDL_PollEvent(&e)) {
     if (e.type == SDL_QUIT) {
       running = false;
     } else {
       const Uint8 *state = SDL_GetKeyboardState(NULL);
-      if (state[SDL_SCANCODE_UP]) paddle.move(true);
-      if (state[SDL_SCANCODE_DOWN]) paddle.move(false);
+      if (state[SDL_SCANCODE_UP]) p.move(true);
+      if (state[SDL_SCANCODE_DOWN]) p.move(false);
     }
   }
 }
