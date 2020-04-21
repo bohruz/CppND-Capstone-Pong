7621ddc87ed31d6f1a38188454ef809cd7b05888
Michael Henrique <oliveirasantos.mh@gmail.com>
2020-04-21 08:53:15 -0300

    fix: Variables name in the class paddle

--- src/paddle.h
+++ src/paddle.h
@@ -7,14 +7,14 @@ class Ball;
 
 class Paddle {
  public:
-  Paddle(int, int, int, int, int, int);
+  Paddle(int x, int y, int h, int w, int bottom, int top);
   virtual void move(bool);
 
-  int x, y, h, w;
-  int score;
+  int x_, y_, h_, w_;
+  int score_;
 
  protected:
-  int top, bottom;
+  int top_, bottom_;
 };
 
 class AutoPaddle : public Paddle {
