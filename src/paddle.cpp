5c7127e94d21df1ee718f7525fb2f06c18cf82cc
Michael Henrique <oliveirasantos.mh@gmail.com>
2020-04-21 10:06:41 -0300

    feat: Paddle implementation

---
+++ src/paddle.cpp
@@ -0,0 +1,28 @@
+#include "paddle.h"
+
+#include "ball.h"
+
+Paddle::Paddle(int x, int y, int w, int h, int bottom, int top)
+    : x_(x), y_(y), w_(w), h_(h), bottom_(bottom), top_(top), score_(0) {}
+
+void Paddle::move(bool upwards) {
+  if (upwards) {
+    y_ -= 5;
+    if (y_ <= top_ + 1) y_ = top_ + 1;
+  } else {
+    y_ += 5;
+    if (y_ >= bottom_ - 1 - h_) y_ = bottom_ - 1 - h_;
+  }
+}
+
+ManualPaddle::ManualPaddle(int x, int y, int w, int h, int bottom, int top)
+    : Paddle(x, y, w, h, bottom, top) {}
+
+AutoPaddle::AutoPaddle(int x, int y, int w, int h, int bottom, int top)
+    : Paddle(x, y, w, h, bottom, top) {}
+
+void AutoPaddle::followBall(const Ball& ball) {
+  float yTarget = ball.y() + 0.5 * ball.h();
+  if (y_ + 0.5 * h_ > yTarget) move(true);
+  if (y_ + 0.5 * h_ < yTarget) move(false);
+}
