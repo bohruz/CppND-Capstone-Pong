b0d921482684a8929f9af57ef56facb24bdcbf33
Michael Henrique <oliveirasantos.mh@gmail.com>
2020-04-21 11:43:27 -0300

    fix: bug in game run function

--- src/game.h
+++ src/game.h
@@ -12,7 +12,7 @@
 class Game {
  public:
   Game(std::size_t grid_width, std::size_t grid_height);
-  void Run(Controller const &controller, Renderer &renderer,
+  void run(Controller const &controller, Renderer &renderer,
            std::size_t target_frame_duration);
 
  private:
