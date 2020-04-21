eb600f6c4ccc6194386e078b3f292ae8ace30c8b
Michael Henrique <oliveirasantos.mh@gmail.com>
2020-04-21 11:29:21 -0300

    feat: game loop completed

--- src/game.cpp
+++ src/game.cpp
@@ -1,89 +1,66 @@
 #include "game.h"
 
 #include <iostream>
+#include <iterator>
+#include <map>
+#include <thread>
 
 #include "SDL.h"
 
 Game::Game(std::size_t grid_width, std::size_t grid_height)
-    : snake(grid_width, grid_height),
-      engine(dev()),
-      random_w(0, static_cast<int>(grid_width)),
-      random_h(0, static_cast<int>(grid_height)) {
-  PlaceFood();
-}
+    : paddleUser_(45, (grid_height - 50) / 2 - 90, 20, 180, 25,
+                  grid_height - 25),
+      paddleMachine_(grid_width - 65, (grid_height - 50) / 2 - 90, 20, 180, 25,
+                     grid_height - 25),
+      ball_((grid_width - 50) / 2 - 15, (grid_height - 50) / 2 - 15, 30, 30, 25,
+            grid_width - 25, 25, grid_height - 25) {}
 
-void Game::Run(Controller const &controller, Renderer &renderer,
+void Game::run(Controller const &controller, Renderer &renderer,
                std::size_t target_frame_duration) {
   Uint32 title_timestamp = SDL_GetTicks();
-  Uint32 frame_start;
-  Uint32 frame_end;
-  Uint32 frame_duration;
+  Uint32 frame_start = 0;
+  Uint32 frame_end = 0;
+  Uint32 frame_duration = 0;
   int frame_count = 0;
   bool running = true;
+
+  std::thread observer([&running, this]() {
+    while (running) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+      std::cout << "Left Score : " << this->p1.score
+                << " - Right Score : " << this->p2.score << std::endl;
+    }
+    std::cout << "Game loop has ended !" << std::endl;
+    std::cout << "Final Left Score : " << this->p1.score
+              << " - Final Right Score : " << this->p2.score << std::endl;
+    std::cout << std::endl;
+  });
 
   while (running) {
     frame_start = SDL_GetTicks();
 
-    // Input, Update, Render - the main game loop.
-    controller.HandleInput(running, snake);
-    Update();
-    renderer.Render(snake, food);
+    p2.follow(b);
+    controller.handleInput(running, p1);
+    b.move(p1, p2);
 
+    renderer.render(p1, p2, b);
     frame_end = SDL_GetTicks();
 
-    // Keep track of how long each loop through the input/update/render cycle
-    // takes.
+    // keep track of how long each loop through the input/update/render cycle
+    // takes
     frame_count++;
     frame_duration = frame_end - frame_start;
 
-    // After every second, update the window title.
+    // Update the window title and keep scores after every second
     if (frame_end - title_timestamp >= 1000) {
-      renderer.UpdateWindowTitle(score, frame_count);
+      renderer.updateWindowTitle(frame_count, p1, p2);
       frame_count = 0;
       title_timestamp = frame_end;
     }
-
-    // If the time for this frame is too small (i.e. frame_duration is
-    // smaller than the target ms_per_frame), delay the loop to
-    // achieve the correct frame rate.
+    // delay the loop to achieve the correct frame rate
     if (frame_duration < target_frame_duration) {
       SDL_Delay(target_frame_duration - frame_duration);
     }
   }
+  observer.join();
 }
-
-void Game::PlaceFood() {
-  int x, y;
-  while (true) {
-    x = random_w(engine);
-    y = random_h(engine);
-    // Check that the location is not occupied by a snake item before placing
-    // food.
-    if (!snake.SnakeCell(x, y)) {
-      food.x = x;
-      food.y = y;
-      return;
-    }
-  }
-}
-
-void Game::Update() {
-  if (!snake.alive) return;
-
-  snake.Update();
-
-  int new_x = static_cast<int>(snake.head_x);
-  int new_y = static_cast<int>(snake.head_y);
-
-  // Check if there's food over here
-  if (food.x == new_x && food.y == new_y) {
-    score++;
-    PlaceFood();
-    // Grow snake and increase speed.
-    snake.GrowBody();
-    snake.speed += 0.02;
-  }
-}
-
-int Game::GetScore() const { return score; }
-int Game::GetSize() const { return snake.size; }
